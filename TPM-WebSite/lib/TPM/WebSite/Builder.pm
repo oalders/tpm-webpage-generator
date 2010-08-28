d978c40b41e8e530a3f88865b1303a757e0fc667
Mike Stok <mike@stok.ca>
2010-08-27 23:05:07 -0400

    Create skeleton for "real" modules.
    Done using module-starter
    --module=TPM::WebSite::Calendar,TPM::WebSite::Meeting,TPM::WebSite::HTMLRenderer,TPM::WebSite::HTMLFile,TPM::WebSite::Builder
    --distro=TPM::WebSite --dir=TPM-WebSite --author="Mike Stok"
    --email="mike@stok.ca"

---
+++ TPM-WebSite/lib/TPM/WebSite/Builder.pm
@@ -0,0 +1,110 @@
+package TPM::WebSite::Builder;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+TPM::WebSite::Builder - The great new TPM::WebSite::Builder!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use TPM::WebSite::Builder;
+
+    my $foo = TPM::WebSite::Builder->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Mike Stok, C<< <mike at stok.ca> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-tpm::website at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TPM::WebSite>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc TPM::WebSite::Builder
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TPM::WebSite>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/TPM::WebSite>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/TPM::WebSite>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/TPM::WebSite/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Mike Stok.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of TPM::WebSite::Builder
---
+++ TPM-WebSite/t/00-load.t
@@ -0,0 +1,18 @@
+#!perl -T
+
+use Test::More tests => 5;
+
+BEGIN {
+    use_ok( 'TPM::WebSite::Calendar' ) || print "Bail out!
+";
+    use_ok( 'TPM::WebSite::Meeting' ) || print "Bail out!
+";
+    use_ok( 'TPM::WebSite::HTMLRenderer' ) || print "Bail out!
+";
+    use_ok( 'TPM::WebSite::HTMLFile' ) || print "Bail out!
+";
+    use_ok( 'TPM::WebSite::Builder' ) || print "Bail out!
+";
+}
+
+diag( "Testing TPM::WebSite::Calendar $TPM::WebSite::Calendar::VERSION, Perl $], $^X" );
